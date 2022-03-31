5b19d6f2236fa7f2adddbe0e0e840165f385c7e7
Eric Nichols <ericnichols@Erics-MBP.lan>
2022-03-31 18:20:59 -0400

    added posts page

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "nextaws": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
